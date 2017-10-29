29a26f39418580a337081c033d2b21705ebca6f4
anabeller <zhuyukunjob@126.com>
2017-10-29 10:53:12 +0800

    Initial commit

---
+++ led/led.c
@@ -0,0 +1,11 @@
+#include"stdio.h"
+
+void led_on(uint led_num)
+{
+	turn_on(led_num);
+}
+
+void led_off(uint led_num)
+{
+	turn_off(led_num);
+}
---
+++ led/led.h
@@ -0,0 +1,9 @@
+#ifndef
+
+void led_on(uint );
+void led_off(uint);
+
+void led_all_on(void);
+
+#endif
+
