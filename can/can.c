68b7924a37a4cfa5467211b1dc36241cf2fc9b21
anabeller <zhuyukunjob@126.com>
2017-10-29 11:19:22 +0800

    增加can总线
    增加can总线文件

---
+++ can/can.c
@@ -0,0 +1,11 @@
+#include"stdio.h"
+
+void can_transimet(uint ch)
+{
+	memcp(ch, "tx84");
+}
+
+void can_reciver(uint ch)
+{
+	turn_off(led_num);
+}
---
+++ can/can.h
@@ -0,0 +1,10 @@
+#ifndef
+
+void led_on(uint );
+void led_off(uint);
+
+void led_all_on(void);
+void led_all_off(void);
+
+#endif
+
